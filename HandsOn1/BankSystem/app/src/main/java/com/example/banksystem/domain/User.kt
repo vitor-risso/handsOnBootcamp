445df38271f1fcc1b8ea84d19ae9b9d8ae7e6854
Vitor Hugo Risso da Silva <CIANDT\vitorhr@lnb028042cps.ciandt.global>
2021-03-30 18:25:41 -0300

    removing the hands on

--- HandsOn1/BankSystem/app/src/main/java/com/example/banksystem/domain/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.banksystem.domain
-
-<<<<<<< HEAD
-class User(val cpf: String, val name: String) {
-
-=======
-class User(var cpf: String, val name: String) {
->>>>>>> 8a1dce093084b07aaa3e251107bc0d8931e63cb5
-}
