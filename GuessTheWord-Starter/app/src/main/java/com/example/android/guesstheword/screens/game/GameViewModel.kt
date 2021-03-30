4bfea0d7b1f6f18fa5deb41f38ea89847fdbd74f
Vitor Hugo Risso da Silva <CIANDT\vitorhr@lnb028042cps.ciandt.global>
2021-03-30 13:26:12 -0300

    [001] refactoring and handling score data

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,4 +1,84 @@
 package com.example.android.guesstheword.screens.game
 
-class GameViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    init {
+        resetList()
+        nextWord()
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        if (wordList.isNotEmpty()) {
+            //Select and remove a word from the list
+            word = wordList.removeAt(0)
+        }
+    }
+
+    /** Methods for buttons presses **/
+    fun onSkip() {
+        if (wordList.size > 0) {
+            if (score > 0) {
+                score--
+                nextWord()
+            }
+        }
+    }
+
+    private val listSize = wordList.size
+    fun onCorrect() {
+        if (score < listSize) {
+            score++
+            nextWord()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
 }
--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.android.guesstheword.screens.score
 
-class ScoreViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+    // The final score
+    var score = finalScore
+    init {
+        Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
 }
