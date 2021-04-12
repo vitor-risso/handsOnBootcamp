3cc3fc40aa75b71b5a7ac1e8d21cf5a8ac4da065
Vitor Hugo Risso da Silva <CIANDT\vitorhr@lnb028042cps.ciandt.global>
2021-04-12 08:39:32 -0300

    .

--- android-workmanager/app/src/main/java/com/example/background/BlurViewModel.kt
+++ android-workmanager/app/src/main/java/com/example/background/BlurViewModel.kt
@@ -19,18 +19,23 @@ package com.example.background
 import android.app.Application
 import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.background.workers.BlurWorker
 
 
 class BlurViewModel(application: Application) : AndroidViewModel(application) {
 
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
+    private val workManager = WorkManager.getInstance(application)
 
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    internal fun applyBlur(blurLevel: Int) {}
 
     private fun uriOrNull(uriString: String?): Uri? {
         return if (!uriString.isNullOrEmpty()) {
@@ -49,5 +54,23 @@ class BlurViewModel(application: Application) : AndroidViewModel(application) {
 
     internal fun setOutputUri(outputImageUri: String?) {
         outputUri = uriOrNull(outputImageUri)
+    }
+
+
+
+    private fun createInputDataForUri(): Data {
+        val builder = Data.Builder()
+        imageUri?.let {
+            builder.putString(KEY_IMAGE_URI, imageUri.toString())
+        }
+        return builder.build()
+    }
+
+    internal fun applyBlur(blurLevel: Int) {
+        val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
+                .setInputData(createInputDataForUri())
+                .build()
+
+        workManager.enqueue(blurRequest)
     }
 }
--- android-workmanager/app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ android-workmanager/app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,41 +2,85 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
-import androidx.work.Worker
-import androidx.work.WorkerParameters
-import com.example.background.R
+import android.net.Uri
+import android.text.TextUtils
+import androidx.work.*
+import com.example.background.KEY_IMAGE_URI
 import timber.log.Timber
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
     override fun doWork(): Result {
         val appContext = applicationContext
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
 
         makeStatusNotification("Blurring image", appContext)
+        sleep()
 
         return try {
-            val picture = BitmapFactory.decodeResource(
-                    appContext.resources,
-                    R.drawable.test)
 
+            if (TextUtils.isEmpty(resourceUri)) {
+                Timber.e("Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+            val resolver = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri)))
             val output = blurBitmap(picture, appContext)
 
-            // Write bitmap to a temp file
             val outputUri = writeBitmapToFile(appContext, output)
 
             makeStatusNotification("Output is $outputUri", appContext)
 
-            Result.success()
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+            val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
+                    .setInputData(createInputDataForUri())
+                    .build()
+
+            val continuation = workManager.beginWith(workA)
+
+            continuation.then(workB) // FYI, then() returns a new WorkContinuation instance
+                    .then(workC)
+                    .enqueue()
+
+            workManager.enqueue(blurRequest)
+
+            Result.success(outputData)
         } catch (throwable: Throwable) {
             Timber.e(throwable, "Error applying blur")
             Result.failure()
         }
     }
 
-//    private val workManager = WorkManager.getInstance(application)
-
     internal fun applyBlur(blurLevel: Int) {
-        workManager.enqueue(OneTimeWorkRequest.from(BlurWorker::class.java))
+        // Add WorkRequest to Cleanup temporary images
+        var continuation = workManager
+                .beginWith(OneTimeWorkRequest
+                        .from(CleanupWorker::class.java))
+
+        // Add WorkRequests to blur the image the number of times requested
+        for (i in 0 until blurLevel) {
+            val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+            // Input the Uri if this is the first blur operation
+            // After the first blur operation the input will be the output of previous
+            // blur operations.
+            if (i == 0) {
+                blurBuilder.setInputData(createInputDataForUri())
+            }
+
+            continuation = continuation.then(blurBuilder.build())
+        }
+
+        // Add WorkRequest to save the image to the filesystem
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+                .build()
+
+        continuation = continuation.then(save)
+
+        // Actually start the work
+        continuation.enqueue()
     }
+
 }
